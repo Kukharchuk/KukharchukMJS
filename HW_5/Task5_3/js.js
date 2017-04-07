bd3bfedaed0b75e7f51204e47ffedb3f4278b781
mkukharchuk <maryakukharchuk@gmail.com>
2017-04-07 12:58:49 +0300

    task_3 after deadline:( Task 1 and 2 I send before deadline

---
+++ HW_5/Task5_3/js.js
@@ -0,0 +1,29 @@
+function random(numb) {
+    return Math.floor(Math.random()*100%numb);
+}
+
+var names = ["Мария", "Валерия", "Виктор", "Надежда", "Анастасия", "Николай"];
+var cities = ["Минск", "Гродно", "Гомель", "Витебск", "Могилев", "Брест"];
+var goods = ['Книга "Цветы для Элджерона"', 'Банка для сыпучих продуктов стеклянная (1,55 л)', 'Сковорода "Мишка"', 'Карта памяти micro SDHC 16Gb', 'Антистресс раскраска "Вторжение дудлов"', 'Книга "Чистый код"']
+var images = ["img/1.jpg", "img/5.jpg", "img/6.jpg", "img/4.jpg", "img/3.jpg", "img/2.jpg"]
+
+
+var body = document.getElementById("body");
+var num = 0, numGoods = 0;
+var I = setInterval(function(){
+    num = random(6);
+    var div = document.createElement("div");
+    body.appendChild(div);
+    div.setAttribute("class","cl"); 
+    var img  = document.createElement("img");
+    div.appendChild(img);
+    img.setAttribute("src", images[num]);
+    img.setAttribute("class","img"); 
+    var p = document.createElement("p");
+    div.appendChild(p);
+    p.innerText = names[random(6)] + " из г." + cities[random(6)] + " купил(а) " + random(20) + " ед. товара " + goods[num];
+    numGoods++;
+    if (numGoods > 9) {
+        clearInterval(I);
+    }
+},2000);
